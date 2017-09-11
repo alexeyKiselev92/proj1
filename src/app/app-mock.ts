abad0d8b5d585486a1c93a0e1372244311a9e17a
alexeyKiselev92 <31734433+alexeyKiselev92@users.noreply.github.com>
2017-09-11 18:20:54 +0300

    fix#1
    fixed troubles with showing data

--- src/app/app-mock.ts
+++ src/app/app-mock.ts
@@ -1,7 +1,8 @@
 import { DiagramItem } from './diagram-item/diagram-item.component';
 
 export const ITEMS:  DiagramItem[] = [
-    {value: 23},
-    {value: 16},
-    {value: 85}
+    {value: 23, name: 'Item1'},
+    {value: 16, name: 'Item2'},
+    {value: 85, name: 'Item3'},
+    {value: 64, name: 'Item4'}
 ]
--- src/app/diagram-item/diagram-item.component.ts
+++ src/app/diagram-item/diagram-item.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core'
+import { Component, Input } from '@angular/core'
 
 @Component({
     selector: 'diagram-item',
@@ -7,5 +7,6 @@ import { Component } from '@angular/core'
 })
 
 export class DiagramItem {
-    value: number;
+   @Input() value: number;
+   @Input() name: string;
 }
